03be140dfa38b95e45dcbeedd9111f1d44986a70
rodrigo <martinqueve60@gmail.com>
2024-11-05 22:01:51 -0300

    reina, incompleto. faltan las diagonales

--- backend-y-poo/Tarea_2024_11_05/ejercicio_8_reinas/ejercicio_8_reinas/Reina.cs
+++ backend-y-poo/Tarea_2024_11_05/ejercicio_8_reinas/ejercicio_8_reinas/Reina.cs
@@ -6,7 +6,97 @@ using System.Threading.Tasks;
 
 namespace ejercicio_8_reinas
 {
-    internal class Reina
+    public class Reina
     {
+        public int col { get; private set; }
+        public int fila { get; private set; }
+
+
+        public Reina(int col, int fila)
+        {
+            this.col = col;
+            this.fila = fila;
+        }
+
+        public Boolean[,] calcularPosicionesVerticalesNoDisponibles(int col)
+        {
+            var posicionesVerticalesNoDisponibles = new Boolean[8, 8];
+
+            for (int filaTablero = 0; filaTablero <= 7; filaTablero++)
+            {
+                posicionesVerticalesNoDisponibles[col, filaTablero] = true;
+            }
+
+            return posicionesVerticalesNoDisponibles;
+        }
+
+        public Boolean[,] calcularPosicionesHorizontalesNoDisponibles(int fila)
+        {
+            var posicionesVerticalesNoDisponibles = new Boolean[8, 8];
+
+            for (int colTablero = 0; colTablero <= 7; colTablero++)
+            {
+                posicionesVerticalesNoDisponibles[colTablero, fila] = true;
+            }
+
+            return posicionesVerticalesNoDisponibles;
+        }
+
+        public Boolean[,] calcularPosicionesDiagonalesNoDisponibles(int col, int fila)
+        {
+            var posicionesDiagonalesNoDisponibles = new Boolean[8, 8];
+
+      
+            calcularDiagonalArribaIzquierda(col, fila, posicionesDiagonalesNoDisponibles);
+            calcularDiagonalArribaDerecha(col, fila, posicionesDiagonalesNoDisponibles);
+
+
+
+            return posicionesDiagonalesNoDisponibles;
+        }
+
+        public void calcularDiagonalArribaIzquierda(int col, int fila, Boolean[,] posiciones)
+        {
+            int filaTablero = fila;
+
+            for (int colTablero = col; colTablero >= 0; colTablero--)
+            {
+                posiciones[colTablero, filaTablero] = true;
+
+                filaTablero++;
+            }
+
+            return;
+        }
+
+        public void calcularDiagonalArribaDerecha(int col, int fila, Boolean[,] posiciones)
+        {
+            int filaTablero = fila;
+
+            for (int colTablero = col; colTablero <= 7; colTablero++)
+            {
+                posiciones[colTablero, filaTablero] = true;
+
+                filaTablero++;
+            }
+
+            return;
+        }
+
+        public void calcularDiagonalAbajoIzquierda(int col, int fila, Boolean[,] posiciones)
+        {
+            int filaTablero = fila;
+
+            for (int colTablero = col; colTablero >= 0 ; colTablero--)
+            {
+                posiciones[colTablero, filaTablero] = true;
+
+                filaTablero--;
+            }
+
+            return;
+        }
+
+
     }
 }
